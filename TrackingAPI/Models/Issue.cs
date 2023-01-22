cb81af3c682f82d7a216808e6cf75ee37d7c978d
rragul <r.ragul0598@gmail.com>
2023-01-22 23:06:07 +0530

    Add project files.

---
+++ TrackingAPI/Models/Issue.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrackingAPI.Models
+{
+	public class Issue
+	{
+		public int Id { get; set; }
+		[Required]
+		public string? Title { get; set; }
+		[Required]
+		public string? Description { get; set; }
+
+		public PriorityEnum Priority { get; set; }
+
+		public IssuTypeEnum IssuType { get; set; }
+
+		public DateTime Created { get; set; }
+		
+		public DateTime? Completed { get; set; }
+		
+		public enum PriorityEnum
+		{
+			Low, Medium, High
+		}
+		
+		public enum IssuTypeEnum
+		{
+			Feature, Bug, Documentation
+		}
+		
+	}
+}
